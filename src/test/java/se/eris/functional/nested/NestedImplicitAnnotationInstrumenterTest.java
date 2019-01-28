482b159db8eeae5eed985142cf16e532897a1d0d
Christian Schwier <Christian.Schwier@ima-zlw-ifu.rwth-aachen.de>
2019-01-28 01:41:33 +0100

    Migrate NestedImplicitAnnotationInstrumenterTest

--- src/test/java/se/eris/functional/nested/NestedImplicitAnnotationInstrumenterTest.java
+++ src/test/java/se/eris/functional/nested/NestedImplicitAnnotationInstrumenterTest.java
@@ -15,51 +15,33 @@
  */
 package se.eris.functional.nested;
 
-import org.junit.jupiter.api.BeforeAll;
-import se.eris.notnull.AnnotationConfiguration;
-import se.eris.notnull.Configuration;
-import se.eris.notnull.ExcludeConfiguration;
-import se.eris.util.ReflectionUtil;
-import se.eris.util.TestClass;
-import se.eris.util.TestCompiler;
-import se.eris.util.TestSupportedJavaVersions;
-import se.eris.util.version.VersionCompiler;
+import se.eris.util.*;
 
-import java.io.File;
 import java.lang.reflect.Method;
-import java.nio.file.Path;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class NestedImplicitAnnotationInstrumenterTest {
 
-    private static final File SRC_DIR = new File("src/test/data");
-    private static final Path DESTINATION_BASEDIR = new File("target/test/data/classes").toPath();
-
-    private static final Map<String, TestCompiler> compilers = new HashMap<>();
-    private static final TestClass testClass = new TestClass("se.eris.nested.TestNestedImplicitAnnotation");
-
-    @BeforeAll
-    static void beforeClass() {
-        final Configuration configuration = new Configuration(true,
-                new AnnotationConfiguration(),
-                new ExcludeConfiguration(Collections.emptySet()));
-        compilers.putAll(VersionCompiler.compile(DESTINATION_BASEDIR, configuration, testClass.getJavaFile(SRC_DIR)));
-    }
-
-    @TestSupportedJavaVersions
-    void anonymousClassConstructor_shouldFollowParentAnnotation(final String javaVersion) throws Exception {
-        final Class<?> c = compilers.get(javaVersion).getCompiledClass(testClass.getName());
-        final Method anonymousClassNullable = c.getMethod("anonymousClassNullable");
+    @InstrumentationConfiguration(implicit = true)
+    @CompiledVersionsTest(sourceClasses = "se.eris.nested.TestNestedImplicitAnnotation")
+    void anonymousClassConstructor_shouldFollowParentAnnotation(final TestCompiler testCompiler, final Class<?> outerClass) throws Exception {
+        final Method anonymousClassNullable = outerClass.getMethod("anonymousClassNullable");
         ReflectionUtil.simulateMethodCall(anonymousClassNullable);
 
-        final Method anonymousClassNotNull = c.getMethod("anonymousClassNotNull");
-        final IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> ReflectionUtil.simulateMethodCall(anonymousClassNotNull));
-        assertEquals("Implicit NotNull argument 0" + VersionCompiler.maybeName(compilers.get(javaVersion), "i") + " of " + testClass.getAsmName() + "$Foo.<init> must not be null", exception.getMessage());
+        final Method anonymousClassNotNull = outerClass.getMethod("anonymousClassNotNull");
+        final IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> ReflectionUtil.simulateMethodCall(anonymousClassNotNull)
+        );
+        assertEquals(
+                String.format(
+                        "Implicit NotNull argument 0%s of %s$Foo.<init> must not be null",
+                        testCompiler.getParameterName("i"),
+                        new TestClass(outerClass.getName()).getAsmName()
+                ), exception.getMessage())
+        ;
     }
 
 }
