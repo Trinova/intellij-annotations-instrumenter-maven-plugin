4afb3ed728d3e28a25b06d4509798e8582650939
Christian Schwier <Christian.Schwier@ima-zlw-ifu.rwth-aachen.de>
2019-01-28 01:48:09 +0100

    Migrate ParameterTypesTest

--- src/test/java/se/eris/types/ParameterTypesTest.java
+++ src/test/java/se/eris/types/ParameterTypesTest.java
@@ -15,58 +15,58 @@
  */
 package se.eris.types;
 
-import org.junit.jupiter.api.BeforeAll;
+import se.eris.util.CompiledVersionsTest;
 import se.eris.util.ReflectionUtil;
 import se.eris.util.TestClass;
 import se.eris.util.TestCompiler;
-import se.eris.util.TestSupportedJavaVersions;
-import se.eris.util.version.VersionCompiler;
 
-import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
-import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@CompiledVersionsTest(sourceClasses = "se.eris.types.TestTypes")
 class ParameterTypesTest {
 
-    private static final File SRC_DIR = new File("src/test/data");
-    private static final Path DESTINATION_BASEDIR = new File("target/test/data/classes").toPath();
-
-    private static final Map<String, TestCompiler> compilers = new HashMap<>();
-    private static final TestClass testClass = new TestClass("se.eris.types.TestTypes");
-
-    @BeforeAll
-    static void beforeClass() {
-        compilers.putAll(VersionCompiler.compile(DESTINATION_BASEDIR, testClass.getJavaFile(SRC_DIR)));
-    }
-
-    @TestSupportedJavaVersions
-    void objectArgument(final String javaVersion) throws Exception {
-        final Class<?> clazz = compilers.get(javaVersion).getCompiledClass(testClass);
-        final Constructor<?> constructor = clazz.getConstructor();
+    @CompiledVersionsTest
+    void objectArgument(final TestCompiler testCompiler, final Class<?> testClass) throws Exception {
+        final Constructor<?> constructor = testClass.getConstructor();
 
         final Object instance = ReflectionUtil.simulateConstructorCall(constructor);
 
         final Method paramObject = instance.getClass().getDeclaredMethod("paramObject", String.class);
-        final IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> ReflectionUtil.simulateMethodCall(instance, paramObject, (Object) null));
-        assertEquals(String.format("NotNull annotated argument 0%s of %s.paramObject must not be null", VersionCompiler.maybeName(compilers.get(javaVersion), "string"), testClass.getAsmName()), exception.getMessage());
+
+        final IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> ReflectionUtil.simulateMethodCall(instance, paramObject, (Object) null)
+        );
+        assertEquals(
+                String.format("NotNull annotated argument 0%s of %s.paramObject must not be null",
+                        testCompiler.getParameterName("string"),
+                        new TestClass(testClass.getName()).getAsmName()
+                ), exception.getMessage()
+        );
     }
 
-    @TestSupportedJavaVersions
-    void arrayArgument(final String javaVersion) throws Exception {
-        final Class<?> clazz = compilers.get(javaVersion).getCompiledClass(testClass);
-        final Constructor<?> constructor = clazz.getConstructor();
+    @CompiledVersionsTest
+    void arrayArgument(final TestCompiler testCompiler, final Class<?> testClass) throws Exception {
+        final Constructor<?> constructor = testClass.getConstructor();
 
         final Object instance = ReflectionUtil.simulateConstructorCall(constructor);
 
         final Method paramObject = instance.getClass().getDeclaredMethod("paramArray", int[].class);
-        final IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> ReflectionUtil.simulateMethodCall(instance, paramObject, (Object) null));
-        assertEquals(String.format("NotNull annotated argument 0%s of %s.paramArray must not be null", VersionCompiler.maybeName(compilers.get(javaVersion), "array"), testClass.getAsmName()), exception.getMessage());
-    }
 
+        final IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> ReflectionUtil.simulateMethodCall(instance, paramObject, (Object) null)
+        );
+        assertEquals(
+                String.format(
+                        "NotNull annotated argument 0%s of %s.paramArray must not be null",
+                        testCompiler.getParameterName("array"),
+                        new TestClass(testClass.getName()).getAsmName()
+                ), exception.getMessage()
+        );
+    }
 }
